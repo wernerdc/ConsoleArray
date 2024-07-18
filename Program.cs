3c899658e2445f6c2fc4a40124a00dd42aba94f8
wernerdc <manuel.werner3@gmx.de>
2024-07-18 15:57:25 +0200

    method fill random array

--- Program.cs
+++ Program.cs
@@ -4,18 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Random r = new Random();
+            
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.Clear();
 
             Console.WriteLine("ConsoleArray \n");
 
-            int[] randomNums = new int[20];
-
-            for (int i = 0; i < randomNums.Length; i++) { 
-                randomNums[i] = r.Next(1, 7);
-            }
+            int[] randomNums = GetRandomNumsArray(20);
 
             for (int i = 0; i < randomNums.Length; i++)
             {
@@ -34,6 +30,19 @@
 
             Console.ReadLine();
 
+        }
+
+        static private int[] GetRandomNumsArray(int size)
+        {
+            Random r = new Random();
+            int[] randomNums = new int[size];
+
+            for (int i = 0; i < randomNums.Length; i++)
+            {
+                randomNums[i] = r.Next(1, 7);
+            }
+
+            return randomNums;
         }
     }
 }
